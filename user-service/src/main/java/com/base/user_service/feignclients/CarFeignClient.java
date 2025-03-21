4bc6c5554fd85d6159c025b7a881c981b46f18fa
Francisco-Robles <franciscojrobles26@gmail.com>
2025-03-21 17:12:35 -0300

    Modificaciones en la comunicacion de los MS para que tomen el nombre del registro

--- user-service/src/main/java/com/base/user_service/feignclients/CarFeignClient.java
+++ user-service/src/main/java/com/base/user_service/feignclients/CarFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "car-service", url = "http://localhost:8002")
+@FeignClient(name = "car-service")
 // @RequestMapping("/cars") -> ya no se usa
 public interface CarFeignClient {
 
