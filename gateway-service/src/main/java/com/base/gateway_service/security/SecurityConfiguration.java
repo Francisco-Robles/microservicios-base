a23ca844122901f63ee7ff9ca5b78e624f5d22b8
Francisco-Robles <franciscojrobles26@gmail.com>
2025-03-27 19:19:08 -0300

    configuracion inicial keycloak

---
+++ gateway-service/src/main/java/com/base/gateway_service/security/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.base.gateway_service.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity httpSecurity){
+
+        httpSecurity.authorizeExchange().anyExchange().authenticated()
+                .and()
+                .oauth2Login(Customizer.withDefaults());
+        httpSecurity.csrf().disable();
+
+        return httpSecurity.build();
+
+    }
+
+}
---
+++ user-service/src/main/java/com/base/user_service/security/UserSecurity.java
@@ -0,0 +1,27 @@
+package com.base.user_service.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity (prePostEnabled = true)
+public class UserSecurity  {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        return http.build();
+    }
+
+}
